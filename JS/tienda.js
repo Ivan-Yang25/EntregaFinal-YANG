b05fbcd489380986b65aa81cb75ce48ff42d8f43
Ivan Yang <yangivan@gmail.com>
2023-11-10 21:58:22 -0300

    Creacion de la base de carrito

--- JS/tienda.js
+++ JS/tienda.js
@@ -3,27 +3,42 @@ document.addEventListener('DOMContentLoaded', () => {
     let usuario = document.getElementById('users');
     let close = document.getElementById('cierre');
     let compra = document.getElementsByClassName('compra');
-    let pokemon = document.getElementByClassName('.card');
+    let pokemon = document.getElementsByClassName('card');
     let carrito = [];
 
     let persona = sessionStorage.getItem("usuarios");
     let personaObj = JSON.parse(persona);
 
-    let {name} = personaObj;
-        
-    usuario.textContent =  `Bienvenido ${name}`;
+    let { name } = personaObj;
 
-    for (let i = 0; i < compra.length; i++){
+    usuario.textContent = `Bienvenido ${name}`;
 
-        compra[i].addEventListener('click', () => {
-            
-            console.log('Compraste');
+    let pokemonObj = function (nombre, clase, precio) {
 
+        this.nombre = nombre;
+        this.clase = clase;
+        this.precio = precio;
+
+    }
+
+    let pokemonObj1;
+
+    for (let u = 0; u < compra.length; u++) {
+
+        compra[u].addEventListener('click', () => {
+
+            let nombre = pokemon[u].getElementsByClassName('nombre')[0].textContent;
+            let clase = pokemon[u].getElementsByClassName('clase')[0].textContent;
+            let precio = pokemon[u].getElementsByClassName('precio')[0].textContent;
+
+            const pokemonObj1 = new pokemonObj(nombre, clase, precio);
+            carrito.push(pokemonObj1);
+            console.log(carrito);
         });
     };
 
     close.addEventListener('click', () => {
 
         window.location = "index.html";
-    }); 
+    });
 });
