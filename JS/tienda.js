3f71f15e66f0cab13d90f4dac9c2b52e49f03c90
Ivan Yang <yangivan@gmail.com>
2023-11-12 17:45:12 -0300

    Finalizando el intento de carrito

--- JS/tienda.js
+++ JS/tienda.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let usuario = document.getElementById('users');
+    let nav = document.getElementById('nav');
     let close = document.getElementById('cierre');
     let compra = document.getElementsByClassName('compra');
     let pokemon = document.getElementsByClassName('card');
+    let items = document.getElementById('compras');
     let carrito = [];
-
     let persona = sessionStorage.getItem("usuarios");
     let personaObj = JSON.parse(persona);
+    let content = document.createElement('DIV');
+    let contentObj = document.createElement('DIV');
+
+
+    nav.appendChild(content);
+
+    items.addEventListener('click', () => {
+        content.classList.contains('visual') ? content.classList.remove('visual') : content.classList.add('visual'); 
+    });
 
     let { name } = personaObj;
 
@@ -29,9 +39,18 @@ document.addEventListener('DOMContentLoaded', () => {
             let clase = pokemon[u].getElementsByClassName('clase')[0].textContent;
             let precio = pokemon[u].getElementsByClassName('precio')[0].textContent;
 
-            const pokemonObj1 = new pokemonObj(nombre, clase, precio);
-            carrito.push(pokemonObj1);
-            console.log(carrito);
+            
+            contentObj.innerHTML = `
+                <h5>
+                    El pokemon es ${nombre}
+                </h5>    
+                <p>
+                    Clase: ${clase}</br>
+                    precio: ${precio}
+                </P>
+            `
+            content.appendChild(contentObj);
+
         });
     };
 
