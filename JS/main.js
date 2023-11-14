cecbc9fdd2a5a6f795e66472a7ae8fe55694810c
Ivan Yang <yangivan@gmail.com>
2023-11-14 20:35:28 -0300

    Mejora del carrito y agregando comentarios

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,42 @@
+//Inicio de JS
+
 document.addEventListener('DOMContentLoaded', () => {
     
+    //Extraer elementos del DOM
     let form = document.getElementById('form');
     let formName = document.getElementById('name');
     let formPass = document.getElementById('password');
     let alerta = document.createElement('DIV');
 
+    //Agregando contenido a la alerta
     alerta.innerHTML = `<p>
                             Complete los campos correspondientes
                         </P>`
 
+    
+    //Agregando evento y preveniento que inicie antes de terminar el codigo
     form.addEventListener('submit', event => {
         event.preventDefault();
 
+        //Extraer el valor de los elementos
         let name = formName.value;
         let password = formPass.value;
 
+        //Funcion para crear un creador de objetos
         function DatosUsuario(name, password){
 
             this.name = name;
             this.password = password;
     
         };
-    
+        
+        //Instanciando un objeto
         const DatosUsuario1 = new DatosUsuario(name, password);
         
+        //Guardando el objeto en un sessionstorage
         sessionStorage.setItem("usuarios", JSON.stringify(DatosUsuario1));
 
+        //Validando inicio de sesion
         if(DatosUsuario1.name === '' && DatosUsuario1.password === '') {
 
             form.appendChild(alerta);
@@ -45,6 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         } else {
             
+            //Envio hacia la otra pagina
             return window.location = "tienda.html";
         };
 
--- JS/tienda.js
+++ JS/tienda.js
@@ -1,5 +1,8 @@
+//Inicio de JS
+
 document.addEventListener('DOMContentLoaded', () => {
 
+    //Llamando elementos del DOM y creacion de arrays
     let usuario = document.getElementById('users');
     let nav = document.getElementById('nav');
     let close = document.getElementById('cierre');
@@ -7,21 +10,29 @@ document.addEventListener('DOMContentLoaded', () => {
     let pokemon = document.getElementsByClassName('card');
     let items = document.getElementById('compras');
     let carrito = [];
+
+    //Llamar info de sessionstorage previamente guardada
     let persona = sessionStorage.getItem("usuarios");
+
+    //Convirtiendo dicha informacion en un objeto
     let personaObj = JSON.parse(persona);
     let content = document.createElement('DIV');
 
-
+    //Anidando elementos
     nav.appendChild(content);
 
+    //Validacion de clase con operador ternario
     items.addEventListener('click', () => {
         content.classList.contains('visual') ? content.classList.remove('visual') : content.classList.add('visual');
     });
 
+    //Destructuring de objeto
     let { name } = personaObj;
 
+    //Agregando contenido al elemento
     usuario.textContent = `Bienvenido ${name}`;
 
+    //Funcion para crear objetos
     let pokemonObj = function (nombre, clase, precio) {
 
         this.nombre = nombre;
@@ -30,21 +41,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    //for para recorrer la lista compra
     for (let u = 0; u < compra.length; u++) {
 
+
+        //Evento para la lista compra
         compra[u].addEventListener('click', () => {
 
+            //Extraer informacion de los elementos en HTML
             let nombre = pokemon[u].getElementsByClassName('nombre')[0].textContent;
             let clase = pokemon[u].getElementsByClassName('clase')[0].textContent;
             let precio = pokemon[u].getElementsByClassName('precio')[0].textContent;
 
+            //Instanciando un nuevo objeto
             const itemCarrito = new pokemonObj(nombre, clase, precio);
             carrito.push(itemCarrito);
 
+            //Declarando la variable contendora del carrito
             let contenidoCarrito ='';
             
+            //Recorriendo los items del carrito
             carrito.forEach(item => {
 
+                //Agregando contenido a la variable
                 contenidoCarrito += `
                     <h5>El Pokemon es: ${item.nombre}</h5></br>
                     <p>
@@ -54,10 +73,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 `
             });
             
+            //Agregando el contenido al elemento del DOM
             content.innerHTML = contenidoCarrito;
         });
     };
 
+    //Volver al incio de sesion
     close.addEventListener('click', () => {
 
         window.location = "index.html";
