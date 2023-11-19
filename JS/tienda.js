1df30f0ce5c05c3bc95cd5d6fa08558079a3d77e
Ivan Yang <yangivan@gmail.com>
2023-11-19 20:51:32 -0300

    Extrayendo los datos de API y modificando el carrito. Inconvenientes con el scope

--- JS/tienda.js
+++ JS/tienda.js
@@ -4,27 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Llamando elementos del DOM y creacion de arrays
     let usuario = document.getElementById('users');
-    let nav = document.getElementById('nav');
     let close = document.getElementById('cierre');
     let compra = document.getElementsByClassName('compra');
-    let pokemon = document.getElementsByClassName('card');
-    let items = document.getElementById('compras');
+    let clear = document.getElementById('clear');
     let carrito = [];
+
 
     //Llamar info de sessionstorage previamente guardada
     let persona = sessionStorage.getItem("usuarios");
 
     //Convirtiendo dicha informacion en un objeto
     let personaObj = JSON.parse(persona);
-    let content = document.createElement('DIV');
+    let content = document.getElementById('content');
 
-    //Anidando elementos
-    nav.appendChild(content);
-
-    //Validacion de clase con operador ternario
-    items.addEventListener('click', () => {
-        content.classList.contains('visual') ? content.classList.remove('visual') : content.classList.add('visual');
-    });
 
     //Destructuring de objeto
     let { name } = personaObj;
@@ -32,51 +24,82 @@ document.addEventListener('DOMContentLoaded', () => {
     //Agregando contenido al elemento
     usuario.textContent = `Bienvenido ${name}`;
 
+
     //Funcion para crear objetos
-    let pokemonObj = function (nombre, clase, precio) {
+    let PokemonObj = function (nombre, clase, precio) {
 
         this.nombre = nombre;
         this.clase = clase;
         this.precio = precio;
 
-    }
-
-    //for para recorrer la lista compra
-    for (let u = 0; u < compra.length; u++) {
-
-
-        //Evento para la lista compra
-        compra[u].addEventListener('click', () => {
-
-            //Extraer informacion de los elementos en HTML
-            let nombre = pokemon[u].getElementsByClassName('nombre')[0].textContent;
-            let clase = pokemon[u].getElementsByClassName('clase')[0].textContent;
-            let precio = pokemon[u].getElementsByClassName('precio')[0].textContent;
-
-            //Instanciando un nuevo objeto
-            const itemCarrito = new pokemonObj(nombre, clase, precio);
-            carrito.push(itemCarrito);
-
-            //Declarando la variable contendora del carrito
-            let contenidoCarrito ='';
-            
-            //Recorriendo los items del carrito
-            carrito.forEach(item => {
-
-                //Agregando contenido a la variable
-                contenidoCarrito += `
-                    <h5>El Pokemon es: ${item.nombre}</h5></br>
-                    <p>
-                        La clase de Pokemon es: ${item.clase}</br>
-                        El precio es: ${item.precio}
-                    </p>
-                `
-            });
-            
-            //Agregando el contenido al elemento del DOM
-            content.innerHTML = contenidoCarrito;
-        });
     };
+
+    //Llamando informacion de API pokemon 
+    const PokemonApi = async () => {
+
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        const data = await response.json();
+        
+
+        for (let i = 0; i < data.results.length; i++) {
+
+            let nombrePok = data.results[i].name;
+
+            let baseURL = "https://pokeapi.co/api/v2/pokemon/";
+
+            let UrlyNombre = baseURL + nombrePok;
+
+            const res = await fetch(UrlyNombre);
+            const datos = await res.json();
+
+            let nombre = datos.name;
+            let clase = datos.types[0].type.name;
+            let precio = datos.weight;
+
+            const PokemonObj1 = new PokemonObj(nombre, clase, precio);
+        };
+
+
+        //for para recorrer la lista compra
+        for (let u = 0; u < compra.length; u++) {
+
+
+            //Evento para la lista compra
+            compra[u].addEventListener('click', () => {
+
+                //Declarando la variable contendora del carrito
+                let contenidoCarrito = '';
+
+                //Agregar el objeto al array
+                carrito.push(PokemonObj1);
+
+                //Recorriendo los items del carrito
+                carrito.forEach(item => {
+
+                    //Agregando contenido a la variable
+                    contenidoCarrito += `
+                <tr class="elemento">
+                    <td>El Pokemon es: ${item.nombre}</td>
+                    <td>La clase del Pokemon es: ${item.clase}</td>
+                    <td>El precio es: ${item.precio}</td>   
+                </tr>
+                `
+                });
+
+                //Agregando el contenido al elemento del DOM
+                content.innerHTML = contenidoCarrito;
+
+                //Vaciar carrito 
+                clear.addEventListener('click', () => {
+                    carrito = [];
+                    content.innerHTML = ``;
+                });
+            });
+        };
+
+    };
+
+    PokemonApi();
 
     //Volver al incio de sesion
     close.addEventListener('click', () => {
